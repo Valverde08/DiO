21bf7ec50b309dada15548d3edec5d1fd9047d6e
Valverde08 <120853222+Valverde08@users.noreply.github.com>
2023-12-20 01:07:40 -0300

    linear gradiente de título

---
+++ Desafio classificador_de_nivel_de_heroi/desafio1.js
@@ -0,0 +1,29 @@
+let nomeHeroi = "vinicius"
+let xpheroi = 2500
+
+if (xpheroi <= 1000) {
+    console.log(`${nomeHeroi} seu badge é bronze`)
+         
+} else if(xpheroi <= 2000){
+    console.log(`${nomeHeroi} seu badge é bronze`)
+         
+} else if(xpheroi <= 5000){
+    console.log(`${nomeHeroi} seu badge é Prata `)
+         
+} else if(xpheroi <= 7000){
+    console.log(`${nomeHeroi} seu badge é Ouro`)
+         
+} else if(xpheroi <= 8000){
+    console.log(`${nomeHeroi} seu badge é Platina`)
+         
+} else if(xpheroi <= 9000){
+    console.log(`${nomeHeroi} seu badge é Ascendente`)
+         
+} else if(xpheroi <= 10000){
+    console.log(`${nomeHeroi} seu badge é Imortal`)
+         
+} else {
+    console.log(`${nomeHeroi} seu badge é Radiante`)
+         
+}
+
