98389f363fe44db6a034996ae2bc2edb463c124a
Valverde08 <120853222+Valverde08@users.noreply.github.com>
2023-12-25 22:43:07 -0300

    atualização final

--- Desafio classificador_de_nivel_de_heroi/desafio1.js
+++ Desafio classificador_de_nivel_de_heroi/desafio1.js
@@ -2,28 +2,28 @@ let nomeHeroi = "vinicius"
 let xpheroi = 2500
 
 if (xpheroi <= 1000) {
-    console.log(`${nomeHeroi} seu badge é bronze`)
+    console.log(`O Herói ${nomeHeroi} está no nível bronze`)
          
 } else if(xpheroi <= 2000){
-    console.log(`${nomeHeroi} seu badge é bronze`)
+    console.log(`O Herói ${nomeHeroi} está no nívelestá no nível bronze`)
          
 } else if(xpheroi <= 5000){
-    console.log(`${nomeHeroi} seu badge é Prata `)
+    console.log(`O Herói ${nomeHeroi} está no nívelPrata `)
          
 } else if(xpheroi <= 7000){
-    console.log(`${nomeHeroi} seu badge é Ouro`)
+    console.log(`O Herói ${nomeHeroi} está no nívelé Ouro`)
          
 } else if(xpheroi <= 8000){
-    console.log(`${nomeHeroi} seu badge é Platina`)
+    console.log(`O Herói ${nomeHeroi} está no nível Platina`)
          
 } else if(xpheroi <= 9000){
-    console.log(`${nomeHeroi} seu badge é Ascendente`)
+    console.log(`O Herói ${nomeHeroi} está no nível Ascendente`)
          
 } else if(xpheroi <= 10000){
-    console.log(`${nomeHeroi} seu badge é Imortal`)
+    console.log(`O Herói ${nomeHeroi} está no nível Imortal`)
          
 } else {
-    console.log(`${nomeHeroi} seu badge é Radiante`)
+    console.log(`O Herói ${nomeHeroi} está no nível Radiante`)
          
 }
 
