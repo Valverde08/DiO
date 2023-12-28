6cf2aedf6065782e261b90f7c24f5a26a8132e7c
Valverde08 <120853222+Valverde08@users.noreply.github.com>
2023-12-28 00:56:42 -0300

    testing

--- Desafio classificador_de_nivel_de_heroi/desafio1.js
+++ Desafio classificador_de_nivel_de_heroi/desafio1.js
@@ -1,29 +1,8 @@
-let nomeHeroi = "vinicius"
-let xpheroi = 2500
-
-if (xpheroi <= 1000) {
-    console.log(`O Herói ${nomeHeroi} está no nível bronze`)
-         
-} else if(xpheroi <= 2000){
-    console.log(`O Herói ${nomeHeroi} está no nívelestá no nível bronze`)
-         
-} else if(xpheroi <= 5000){
-    console.log(`O Herói ${nomeHeroi} está no nívelPrata `)
-         
-} else if(xpheroi <= 7000){
-    console.log(`O Herói ${nomeHeroi} está no nívelé Ouro`)
-         
-} else if(xpheroi <= 8000){
-    console.log(`O Herói ${nomeHeroi} está no nível Platina`)
-         
-} else if(xpheroi <= 9000){
-    console.log(`O Herói ${nomeHeroi} está no nível Ascendente`)
-         
-} else if(xpheroi <= 10000){
-    console.log(`O Herói ${nomeHeroi} está no nível Imortal`)
-         
-} else {
-    console.log(`O Herói ${nomeHeroi} está no nível Radiante`)
-         
+function meuNome(nome){
+    console.log(`Bem-Vindo ${nome}`)
 }
+
+(function main() {
+    meuNome();
+})()
 
--- Formação Java Script Developer/exercicios.js
+++ Formação Java Script Developer/exercicios.js
@@ -1,18 +1,23 @@
-let tipo = 1
-let valor = 1500
 
-
-
-if(tipo === 1){
-    valorFinal = valor - (valor * 0.10)
-    console.log(`como a sua forma de pagamento será o débito você terá 10% de desconte e pagará ${valorFinal}`)
-} else if(tipo === 2){
-    valorFinal = valor - (valor * 0.15)
-    console.log(`como a sua forma de pagamento será o PIX você terá 15% de desconte e pagará ${valorFinal}`)
-} else if(tipo === 3){
-    valorFinal = valor
-    console.log(`como a sua forma de pagamento será o parcelamento você terá 15% de desconte e pagará ${valorFinal}`)
-} else {
-    valorFinal = valor + (valor * 0.10)
-    console.log(`como a sua forma de pagamento será o parcelamento você terá 15% de desconte e pagará ${valorFinal}`)
+function fazerDesconto(valor,pagamento){
+    
+    if(pagamento === 1){
+        let total = valor-(valor * 0.10)
+        console.log(`No débito você possui 10% de desconto. tendo valor total de ${total}`)
+    } else if(pagamento === 2){
+        let total = valor-(valor * 0.15)
+        console.log(`No PIX você possui 15% de desconto. tendo valor total de ${total}`)
+    } else if(pagamento === 3 ){
+        let total = valor
+        console.log(`No parcelado em duas vezes você não possui 1desconto. tendo valor total de ${total}`)
+    } else {
+        let total = valor+(valor * 0.10)
+        console.log(`No parcelado em mais de duas vezes você não possui desconto e tem um acréscimo de 10%. Sendo o valor total de ${total}`)
+    }
 }
+
+
+
+(function main() {
+    fazerDesconto(500, 2)
+})()
