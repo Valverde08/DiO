57ba6332362911847069322149be5773c3997eac
Valverde08 <120853222+Valverde08@users.noreply.github.com>
2023-12-31 01:58:20 -0300

    criando objetos

--- Desafio classificador_de_nivel_de_heroi/desafio2.js
+++ Desafio classificador_de_nivel_de_heroi/desafio2.js
@@ -1,24 +1,18 @@
-function calcularVitorias(vitoria,derrota){
-    let saldo =(vitoria-derrota)
+class Carro{
+    marca
+    cor
+    gastoMedio
+
+    constructor(marca, cor , gastoMedio){
+        this.marca = marca
+        this.ano = cor
+        this.cor = gastoMedio
+
+    }
+
     
-    if(saldo <= 10){
-        console.log(`O heroi tem o saldo de ${saldo} vitorias e está no nível Ferro`)
-    } else if(saldo > 11 && saldo <= 20){
-        console.log(`O heroi tem o saldo de ${saldo} vitorias e está no nível bronze`)
-    }  else if(saldo > 20 && saldo <= 50){
-        console.log(`O heroi tem o saldo de ${saldo} vitorias e está no nível Prata`)
-    }  else if(saldo > 50 && saldo <= 80){
-        console.log(`O heroi tem o saldo de ${saldo} vitorias e está no nível Ouro`)
-    }  else if(saldo > 80 && saldo <= 90){
-        console.log(`O heroi tem o saldo de ${saldo} vitorias e está no nível Diamante`)
-    }  else if(saldo > 90 && saldo <= 100){
-        console.log(`O heroi tem o saldo de ${saldo} vitorias e está no nível Lendario`)
-    }  else {
-        console.log(`O heroi tem o saldo de ${saldo} vitorias e está no nível Imortal`)
-    }  
 }
 
-(function main(){
-    calcularVitorias(70,10)
-
-})()
+const ferrari = {
+    
+}
--- Formação Java Script Developer/exercicios.js
+++ Formação Java Script Developer/exercicios.js
@@ -1,23 +1,19 @@
+class Pessoa {
+    nome
+    age
+    weight
+    height
 
-function fazerDesconto(valor,pagamento){
-    
-    if(pagamento === 1){
-        let total = valor-(valor * 0.10)
-        console.log(`No débito você possui 10% de desconto. tendo valor total de ${total}`)
-    } else if(pagamento === 2){
-        let total = valor-(valor * 0.15)
-        console.log(`No PIX você possui 15% de desconto. tendo valor total de ${total}`)
-    } else if(pagamento === 3 ){
-        let total = valor
-        console.log(`No parcelado em duas vezes você não possui 1desconto. tendo valor total de ${total}`)
-    } else {
-        let total = valor+(valor * 0.10)
-        console.log(`No parcelado em mais de duas vezes você não possui desconto e tem um acréscimo de 10%. Sendo o valor total de ${total}`)
+
+    constructor(name, age,weight, height ){
+        this.name = name
+        this.age = age
+        this.weight = weight
+        this.height = height
+        
+    }
+
+    calcularImc() {
+
     }
 }
-
-
-
-(function main() {
-    fazerDesconto(500, 2)
-})()
