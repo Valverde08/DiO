03ec518128f86af4f561158b858b7d1a5e243167
Valverde08 <120853222+Valverde08@users.noreply.github.com>
2024-01-04 23:28:58 -0300

    atividade java script

--- Formação Java Script Developer/exercicios.js
+++ Formação Java Script Developer/exercicios.js
@@ -1,42 +1,15 @@
-class Pessoa {
-    nome
-    age
-    weight
-    height
 
 
-    constructor(name, age,weight, height ){
-        this.name = name
-        this.age = age
-        this.weight = weight
-        this.height = height
+let numeros = [2 , 5, 10, 100, 25, 30, 32, 25]
+
+function numPares(){
+    for (let i = 0; i <= numeros.length; i++) {
+        if(numeros[i] % 2 == 0){
+            console.log(`${numeros[i]} é par `)
+        } 
         
     }
 
-    calcularImc() {
-        return this.weight /(this.height * this.height)
-
-    }
-    classificarImc(){
-        const imc = this.calcularImc()
-        if(imc < 18.5){
-            return(`abaixo do peso. Seu imc é de ${imc.toFixed(2)}`)
-
-        } else if(imc >= 18.5 && imc < 24.9){
-            return(`está com o peso ideal. Seu imc é de ${imc.toFixed(2)}`)
-        } else if(imc >= 24.9 && imc < 29.9){
-            return(`está um pouco acima do peso ideal. Seu imc é de ${imc.toFixed(2)}`)
-        } else if(imc >= 30 && imc < 40){
-            return(`VocÊ está com obesidade . Seu imc é de ${imc.toFixed(2)}`)
-        } else {
-            return(`VocÊ está com obesidade Morbida . Seu imc é de ${imc.toFixed(2)}`)
-        }
-
-    }
 }
 
-const vinicius = new Pessoa ("Vinicius", 28, 75, 1.83)
-
-console.log(vinicius)
-
-console.log(vinicius.classificarImc())
+numPares(10)
