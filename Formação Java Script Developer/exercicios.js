10e2dfa0fb755819ee2135ca24251161fdb05ab7
Vinicius <valverde88@outlook.com>
2024-01-08 22:14:25 -0300

    testes

--- Formação Java Script Developer/exercicios.js
+++ Formação Java Script Developer/exercicios.js
@@ -1,20 +1,6 @@
 const {gets, print} = require('./funcoes-iniciais')
 
-const array = [10, 51,  3, 75, 36, 89, 99]
 
-let maiorNumPar = 0 
-let maiorNumImpar = 0
-const num = array.length
-for (let i = 0; i < num; i++) {
-    const element = array[i]
-    if( element > maiorNumPar && element % 2 === 0){
-         maiorNumPar = element
-    } else if (element > maiorNumImpar && element % 2 === 1){
-         maiorNumImpar = element
-    }
-    
-}
+const salario = gets(1000, 250)
 
-
-print(maiorNumPar)
-print(maiorNumImpar)
+print(salario)
--- Formação Java Script Developer/funcoes-iniciais.js
+++ Formação Java Script Developer/funcoes-iniciais.js
@@ -1,12 +1,17 @@
 const num = [10, 51]
 let i = 0
 
-function gets(){
-    const valor = num[i]
-    i++
-    return valor
+function gets(salario, adicional){
+    if(salario <= 1100){
+        return (  salario - (salario * 0.05)) + adicional
+    } else if (salario > 1100  && salario <= 2500){
+        return ( salario - (salario * 0.10)) + adicional
+    } else {
+        return (salari -(salario * 0.15)) + adicional
+    }
+    
 
-}
+} 
 
 function print(text){
     console.log(text)
