f79d4ce40652f51c549bc15a030d6b42d7a3adff
Vinicius <valverde88@outlook.com>
2024-01-31 01:39:55 -0300

    desafio, começo

---
+++ Projetos praticos/plaindromo/main.js
@@ -0,0 +1,23 @@
+let frase = "agua"
+// let dividido = frase.split("")
+let revertido = frase.split("")
+
+console.log(frase.split())
+console.log(revertido.reverse()[1])
+
+
+
+// function checarPalavra(){
+//     if(dividido === dividido.reverse()){
+//         console.log("A sua frase é um palindromo ")
+//     } else{
+//         console.log("A frase não é um palindromo")
+//     }
+// }
+
+
+
+// for (let i = 0; i < dividido.length; i++) {
+    
+//     console.log(dividido[i])
+// }
