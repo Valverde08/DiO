656d528ef21e2047e8834f0c2dbdbe858939317c
Vinicius <valverde88@outlook.com>
2024-02-06 00:20:53 -0300

    .

--- Projetos praticos/plaindromo/main.js
+++ Projetos praticos/plaindromo/main.js
@@ -1,21 +1,8 @@
-let frase = "se fuder"
+function verificarPalindromo(string){
+    if(!string) return;
 
-
-let dividido = frase.replace(/ /g, "")
-let divididores = dividido.split("")
-
-
-console.log(dividido) 
-console.log(divididores) 
-
-
-let revertido = divididores.reverse()
-console.log(revertido) 
-
-for (let i = 0; i < divididores.length; i++) {
-    console.log(divididores[i])
-    
+    return string.split("").reverse().join("") === string
     
 }
 
-
+console.log(verificarPalindromo("pica"))
